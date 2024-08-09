d7606acecb4f32ba6ed2d4f726e429510836d970
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-08-10 00:46:52 +0900

    seconf

--- nogitu/header.php
+++ nogitu/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Gorge Portfolio Template</title>	
 	<meta charset="UTF-8">
-	<meta name="description" content="Gorge Portfolio Template">
+	<meta name="description" content="乃木坂46の切り抜き動画のキュレーションメディアです。">
 	<meta name="keywords" content="personal, portfolio">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.png">
 </head>
 <?php wp_head();?>
